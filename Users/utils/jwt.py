baf1924c3acfebacd9e22e10b4c382522bdd1235
aracheldog <zouzhicheng2001@outlook.com>
2023-11-14 16:07:57 -0500

    add function to decode the token generated by the google service account

--- Users/utils/jwt.py
+++ Users/utils/jwt.py
@@ -1,7 +1,7 @@
 import google.auth.crypt
 import google.auth.jwt
 import time
-import requests
+
 
 def generate_jwt(
     sa_keyfile,
@@ -30,7 +30,10 @@ def generate_jwt(
     # sign with keyfile
     signer = google.auth.crypt.RSASigner.from_service_account_file(sa_keyfile)
     jwt = google.auth.jwt.encode(signer, payload)
+    payload = google.auth.jwt.decode(jwt, verify=False)
     print("Bearer {}".format(jwt.decode("utf-8")))
+    print()
+    print("decoded payload is: ", payload)
     return jwt
 
 if __name__ == '__main__':
